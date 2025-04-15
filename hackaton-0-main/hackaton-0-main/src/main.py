313410a972c327b41be6f4ec389b2bcec23095f3
FernandoEspi <166545241+FernandoEspi@users.noreply.github.com>
2025-04-15 17:54:41 -0500

    Add files via upload

---
+++ hackaton-0-main/hackaton-0-main/src/main.py
@@ -0,0 +1,33 @@
+def calculadora(num1, num2 , operacion):
+    while True:
+        try:
+            if operacion == "+":
+                resultado = num1 + num2
+            elif operacion == "-":
+                resultado = num1 - num2
+            elif operacion == "*":
+                resultado = num1 * num2
+            elif operacion == "/":
+                if num2 == 0:
+                    print("Error: No se puede dividir entre cero.")
+                    continue
+                resultado = num1 / num2
+            else:
+                print("Operación no válida. Intenta nuevamente.")
+                continue
+
+            print(f"Resultado: {resultado}")
+
+            otra = input("¿Deseas hacer otra operación? Responde un (si)").lower()
+            if otra != "si":
+                print("Gracias por usar la calculadora.")
+                break
+
+        except ValueError:
+            print("Entrada inválida. Por favor ingresa números válidos.")
+
+num1 = float(input("ingrese un numero"))
+num2 = float(input("Ingrese otro numero"))
+operacion = str(input("Ingrese operación"))
+
+calculadora(num1 , num2, operacion)
